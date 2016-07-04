32461e0e62d26d23f5525a8491425e390b06aaa9
make92 <mm92@gmx.com>
2016-07-04 23:11:55 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,11 +21,26 @@ rm -rf /etc/service/sshd /etc/my_init.d/00_regen_ssh_host_keys.sh
 add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ trusty universe multiverse"
 add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ trusty-updates universe multiverse"
 add-apt-repository "deb http://apt.sonarr.tv/ master main"
+apt-add-repository ppa:mc3man/trusty-media -y
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys FDA5DFFC
 
 # Install Dependencies
 apt-get update -qq
 apt-get install -qy libgdiplus libmono-cil-dev nzbdrone
+apt-get install python-pip -y
+apt-get install ffmpeg
+curl https://bootstrap.pypa.io/ez_setup.py -O
+python3.4 ez_setup.py
+pip install requests 
+pip install requests[security] 
+pip install requests-cache 
+pip install babelfish 
+pip install "guessit<2"
+pip install subliminal
+pip install deluge-client
+pip install qtfaststart
+
+
 
 #########################################
 ##             INSTALLATION            ##
@@ -33,6 +48,7 @@ apt-get install -qy libgdiplus libmono-cil-dev nzbdrone
 
 # Fix permissions
 chown -R nobody:users /opt/NzbDrone
+ln -sf /opt/ffmpeg/bin/ffmpeg /usr/bin/ffmpeg
 
 #########################################
 ##                 CLEANUP             ##
